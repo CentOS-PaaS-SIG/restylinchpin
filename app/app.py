0dc0ff213e2b553e3085335a112dce4af0ef19cb
mansikulkarni96 <mansikul26@gmail.com>
2019-06-04 23:00:11 -0400

    Added API for creating workspaces

--- app/app.py
+++ app/app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from os import path
+import subprocess
+import json
+import os
 
 app = Flask(__name__)
 
+with open('config.json') as json_data_file:
+    config = json.load(json_data_file)
+    workspace_dir = config["path1"]
 
-@app.route('/hello')
-def index():
-    return "Hello, World!"
+
+@app.route('/workspace/create', methods=['POST'])
+def linchpin_init():
+    data = request.json
+    name = data["name"]
+    cmd = "linchpin init" + " " + name
+    os.chdir(workspace_dir)
+    if path.isdir(name):
+        return jsonify(status="Workspace already exists")
+    else:
+        subprocess.run(cmd, shell=True)
+        return jsonify(name=data["name"], status="Workspace created successfully")
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()

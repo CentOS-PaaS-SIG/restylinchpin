9d6a1b46bc1c080e9fab518771150a91ca5698fa
mansikulkarni96 <mansikul26@gmail.com>
2019-06-05 20:47:53 -0400

    Updated changes for PR:added yml file, added -w flag, added exception handling and return code

--- app/app.py
+++ app/app.py
@@ -1,30 +1,38 @@
-from flask import Flask, jsonify, request
-from os import path
+from flask import Flask, jsonify, request, Response
 import subprocess
-import json
 import os
+import yaml
 
 app = Flask(__name__)
 
 # Reading directory path from config.json file
-with open('config.json') as json_data_file:
-    config = json.load(json_data_file)
-    WORKSPACE_DIR = config["path1"]
+
+with open('config.yml', 'r') as f:
+    doc = yaml.load(f)
+
+WORKING_DIR = doc['working_path']
 
 # Route for creating workspaces
 @app.route('/workspace/create', methods=['POST'])
 def linchpin_init():
-    data = request.json     # Get request body
-    name = data["name"]
-    cmd = "linchpin init" + " " + name
-    os.chdir(WORKSPACE_DIR)  # Change directory to the one set in config.json
-    if path.isdir(name):
-        return jsonify(status="Workspace already exists")   # Checking if workspace already exists
-
-    else:
-        subprocess.run(cmd, shell=True)  # run the command linchpin init on command line
-        return jsonify(name=data["name"], status="Workspace created successfully")
+    try:
+        data = request.json     # Get request body
+        name = data["name"]
+        if os.path.exists(WORKING_DIR + "/" + name):
+            return jsonify(status="Workspace already exists")   # Checking if workspace already exists
+        else:
+            output = subprocess.Popen(["linchpin", "-w " + WORKING_DIR + name + "/",  "init"], stdout=subprocess.PIPE)
+            return jsonify(name=data["name"], status="Workspace created successfully", Code=output.returncode)
+    except Exception as e:
+        print(e)
+        return jsonify(status=409, code=output.returncode)
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
+
+
+
+
+
+

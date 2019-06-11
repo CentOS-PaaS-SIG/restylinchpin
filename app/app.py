e691c3102d92b4487ebff51d99eb834ede112b5f
mansikulkarni96 <mansikul26@gmail.com>
2019-06-11 11:44:10 -0400

    Chnaged exception print statements to flask based loggers

--- app/app.py
+++ app/app.py
@@ -5,6 +5,8 @@ import yaml
 
 import shutil
 import json
+import logging
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 
@@ -27,7 +29,7 @@ def linchpin_init():
             output = subprocess.Popen(["linchpin", "-w " + WORKING_DIR + name + "/",  "init"], stdout=subprocess.PIPE)
             return jsonify(name=data["name"], status="Workspace created successfully", Code=output.returncode)
     except Exception as e:
-        print(e)
+        app.logger.error(e)
         return jsonify(status=409, code=output.returncode)
 
 # Route for listing all workspaces
@@ -42,7 +44,7 @@ def linchpin_list_workspace():
                 workspace_array.append(workspace_dict)
         return Response(json.dumps(workspace_array), status=200, mimetype='application/json')
     except Exception as e:
-        print(e)
+        app.logger.error(e)
         return jsonify(status=409, message=str(e))
 
 
@@ -63,6 +65,9 @@ def linchpin_delete_workspace():
 
 
 if __name__ == "__main__":
+    handler = RotatingFileHandler('restylinchpin.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
     app.run(host='0.0.0.0', debug=True)
 
 

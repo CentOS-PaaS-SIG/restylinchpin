eb37a87817fa611025e6fe9e3129120df6a6eeaa
Mansi <kukarni.man@husky.neu.edu>
2019-06-21 10:13:23 -0400

    Revert "Merge branch 'refactor' into packaging"
    This reverts commit f993d3b212336c8f724ed1b7a88e8f8f791e7366.

--- config/errors.py
+++
@@ -1,6 +0,0 @@
-KEY_ERROR_NAME = "Please provide request body in format:{name: workspacename}"
-KEY_ERROR_PARAMS = "Please provide request body in format:" \
-                 "{name: workspacename, url: someurl, " \
-                 "rootfolder:/path/to/folder}"
-ERROR_STATUS = 409
-
